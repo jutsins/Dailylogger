d6b097bdc4d038ee8c241846a23fdeb5851ef976
Justin Laptop <jutsins@gmail.com>
2019-09-26 18:36:04 +0200

    update gsheet
    now stores token in separate folder

--- src/gsheet/gsheet_api.py
+++ src/gsheet/gsheet_api.py
@@ -21,8 +21,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('gsheet/token.pickle'):
+        with open('gsheet/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -33,7 +33,7 @@ def main():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('gsheet/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
